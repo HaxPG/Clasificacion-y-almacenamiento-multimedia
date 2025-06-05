0e741948bd46bba9e042838b5a1b34cae95d4027
Nain Felipe Burgos Ortiz <burgosnain2@gmail.com>
2025-06-05 17:48:49 -0400

    asada

--- multimedia-backend/server.js
+++ multimedia-backend/server.js
@@ -33,7 +33,7 @@ const dbConfig = {
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "Multimedia",
+  database: process.env.DB_NAME || "multimedia",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
