c3e13eb91fffb2beeb2b1b17548395426c519d67
Nain Felipe Burgos Ortiz <burgosnain2@gmail.com>
2025-06-20 12:38:18 -0400

    cambios front

--- Clasificacion-y-almacenamiento-multimedia/src/app/shared/sidebar/sidebar.ts
+++ Clasificacion-y-almacenamiento-multimedia/src/app/shared/sidebar/sidebar.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  templateUrl: './sidebar.html',
+  styleUrls: ['./sidebar.css']
 })
 export class SidebarComponent {
   isCollapsed = false;
